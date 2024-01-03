3c9ba90208e90818510a0b135119bad83a7f8ad0
jagdish <jagdish.funn@gmail.com>
2024-01-04 00:29:37 +0530

    fix : upload attachment

--- client/src/components/forms/customInputs/uploadFile.js
+++ client/src/components/forms/customInputs/uploadFile.js
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
-export default function InputFileUpload() {
-  const [selectedFile, setSelectedFile] = React.useState(null);
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    console.log('Selected file:', file);
-    setSelectedFile(file);
-  };
-
+export default function InputFileUpload({ onChange, file }) {
   return (
-    <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
-      {selectedFile ? `File: ${selectedFile.name}` : 'Upload file'}
-      <VisuallyHiddenInput type="file" accept=".doc, .docx, .zip, .jpg, .jpeg, .png, .xlsx, .xls,.pdf" onChange={handleFileChange} />
+    <Button
+      component="label"
+      variant="contained"
+      startIcon={<CloudUploadIcon />}
+    >
+      {file ? `File: ${file.name}` : "Upload file"}
+      <VisuallyHiddenInput
+        type="file"
+        accept=".doc, .docx, .zip, .jpg, .jpeg, .png, .xlsx, .xls,.pdf"
+        onChange={onChange}
+      />
     </Button>
   );
 }
