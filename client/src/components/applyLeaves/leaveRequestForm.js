8b71c5fc07ed06b34ac9b52bb1de8b84ee7e439d
jagdish <jagdish.funn@gmail.com>
2024-01-06 13:19:39 +0530

    new : added popup LOP select

--- client/src/components/applyLeaves/leaveRequestForm.js
+++ client/src/components/applyLeaves/leaveRequestForm.js
@@ -23,6 +23,7 @@ const LeaveRequestForm = ({
   file,
   errors,
   setErrors,
+  // hasNumberDaysGreaterThanZero,
 }) => {
   const { numberOfDays } = useSelector((state) => state?.nonPersist.leavesData);
   const [status, setStatus] = useState("Saved");
@@ -112,7 +113,6 @@ const LeaveRequestForm = ({
 
     // Usage example
     const privilegeLeaveId = findLeaveMasterId("Privilege Leave");
-    console.log("privilegeLeaveId", privilegeLeaveId);
     const paternityLeaveId = findLeaveMasterId("Paternity Leave");
     const compensatoryLeaveId = findLeaveMasterId("Compensatory Leave");
     const maternityLeaveId = findLeaveMasterId("Maternity Leave");
@@ -262,8 +262,8 @@ const LeaveRequestForm = ({
                     border: "1px solid #8897ad87",
                     borderRadius: "5px",
                     textAlign: "center",
-                    height:'29.8%',
-                    top:'-0.5px'
+                    height: "29.8%",
+                    top: "-0.5px",
                   }}
                 />
                 {errors.fromSession && (
@@ -290,9 +290,9 @@ const LeaveRequestForm = ({
                     ...style.TimesheetTextField,
                     border: "1px solid #8897ad87",
                     borderRadius: "5px",
-                    height:'29.6%',
+                    height: "29.6%",
                     textAlign: "center",
-                    top:'3.8px'
+                    top: "3.8px",
                   }}
                 />
                 {errors.toSession && (
