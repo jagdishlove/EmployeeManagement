9b7bd8bf3a516f9cba808ad06f44f08cf0c2704b
yogita <yogita@prakat.in>
2024-06-06 14:27:51 +0530

    latest code

--- client/src/pages/reports/UsersReportsTable.js
+++ client/src/pages/reports/UsersReportsTable.js
@@ -99,178 +99,207 @@ const ReportsTable = ({
               </TableRow>
             </TableHead>
             <TableBody sx={{ textAlign: "center" }}>
-              {data?.map((row, index) => (
-                <React.Fragment key={row.sNo}>
-                  <TableRow key={index}>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {index + 1}
-                    </TableCell>
-                    <TableCell>
-                      <Grid container alignItems="center" spacing={1}>
-                        <Avatar
-                          sx={{ color: "#fff", backgroundColor: "#4813B8" }}
-                        >
-                          {row.fullName.charAt(0).toUpperCase()}
-                        </Avatar>
-                        <Typography style={{ fontSize: "13px" }}>
-                          {row.fullName}
-                        </Typography>
-                      </Grid>
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.employeeId}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.employmentType}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center",
-                          width: {
-                            xs: "100%", // Full width on small screens
-                            sm: "200px", // Specific width on medium and larger screens
-                            md: "170px",
-                          },
-                          height: "40px",
-                          padding: "0 8px",
-                        }}
-                      >
-                        <Box
-                          sx={{
-                            overflow: "hidden",
-                            textOverflow: "ellipsis",
-                            whiteSpace: "nowrap",
-                            display: "flex",
-                            alignItems: "center",
-                            flex: 1,
-                          }}
-                        >
-                          {row?.projectNames
-                            .slice(0, 1)
-                            ?.map((project, projectIndex) => (
-                              <Grid
-                                item
-                                key={projectIndex}
-                                sx={{
-                                  border: "1px solid #AEAEAE",
-                                  borderRadius: "8px",
-                                  padding: "10px",
-                                  margin: "5px",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  justifyContent: "center",
-                                  color: "#000000",
-                                  backgroundColor: "#ffffff",
-                                }}
-                              >
-                                {project}
-                              </Grid>
-                            ))}
-                        </Box>
-                        {/* Conditionally render the button */}
-                        {row.projectNames.length > 1 && (
-                          <Button
-                            variant="text"
-                            onClick={() => toggleProjectExpansion(index)}
+              {timesheetReports?.timesheetreportsDetails?.content?.length >
+              0 ? (
+                <>
+                  {data?.map((row, index) => (
+                    <React.Fragment key={row.sNo}>
+                      <TableRow key={index}>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {index + 1}
+                        </TableCell>
+                        <TableCell>
+                          <Grid container alignItems="center" spacing={1}>
+                            <Avatar
+                              sx={{ color: "#fff", backgroundColor: "#4813B8" }}
+                            >
+                              {row.fullName.charAt(0).toUpperCase()}
+                            </Avatar>
+                            <Typography style={{ fontSize: "13px" }}>
+                              {row.fullName}
+                            </Typography>
+                          </Grid>
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.employeeId}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.employmentType}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          <Box
                             sx={{
-                              fontSize: "20px",
-                              color: "#1475E7",
-                              paddingRight: "8px",
-                              minWidth: "auto", // Adjust button width to fit content
+                              display: "flex",
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                              width: {
+                                xs: "100%", // Full width on small screens
+                                sm: "200px", // Specific width on medium and larger screens
+                                md: "170px",
+                              },
+                              height: "40px",
+                              padding: "10 8px",
+                              backgroundColor: "#f6f6f6",
                             }}
                           >
-                            {expandedProjects.includes(index) ? (
-                              <KeyboardArrowUpIcon />
-                            ) : (
-                              <KeyboardArrowDownIcon />
+                            <Box
+                              sx={{
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                whiteSpace: "nowrap",
+                                display: "flex",
+                                alignItems: "center",
+                                flex: 1,
+                               
+                              }}
+                            >
+                              {row?.projectNames
+                                .slice(0, 1)
+                                ?.map((project, projectIndex) => (
+                                  <Grid
+                                    item
+                                    key={projectIndex}
+                                    sx={{
+                                      border: "1px solid #AEAEAE",
+                                      borderRadius: "8px",
+                                      padding: "5px",
+                                      margin: "5px",
+                                      display: "flex",
+                                      alignItems: "center",
+                                      justifyContent: "center",
+                                      color: "#000000",
+                                      backgroundColor: "#ffffff",
+                                    }}
+                                  >
+                                    {project}
+                                  </Grid>
+                                ))}
+                            </Box>
+                            {/* Conditionally render the button */}
+                            {row.projectNames.length > 1 && (
+                              <Button
+                                variant="text"
+                                onClick={() => toggleProjectExpansion(index)}
+                                sx={{
+                                  fontSize: "20px",
+                                  color: "#1475E7",
+                                  paddingRight: "8px",
+                                  minWidth: "auto", // Adjust button width to fit content
+                                }}
+                              >
+                                {expandedProjects.includes(index) ? (
+                                  <KeyboardArrowUpIcon />
+                                ) : (
+                                  <KeyboardArrowDownIcon />
+                                )}
+                              </Button>
                             )}
-                          </Button>
-                        )}
-                      </Box>
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.noOfWorkingDays}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.totalWorkingDays}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.loggedInHours}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.ratings}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.variablePay}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "13px" }}>
-                      {row.lossOfPay}
-                    </TableCell>
-                    <TableCell style={{ fontSize: "16px" }}>
-                      <Button onClick={() => handleDownload(row.downloadLink)}>
-                        <SimCardDownloadOutlinedIcon />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                  {expandedProjects.includes(index) &&
-                  row.projectNames.length > 1 ? (
-                    <TableRow sx={{ border: "5px solid #EBEBEB" }}>
-                      <TableCell colSpan={12}>
-                        <Grid
-                          container
-                          alignItems="center"
-                          justifyContent="space-between"
-                          sx={{
-                            backgroundColor: "#fff",
-                            padding: "5px",
-                          }}
-                        >
-                          <Grid item display={"flex"} flexDirection={"row"}>
-                            {row.projectNames
-                              .slice(1)
-                              .map((projectName, index) => (
-                                <Typography
-                                  key={index} // Don't forget to provide a unique key if you're rendering a list
-                                  variant="body1"
-                                  sx={{
-                                    border: "1px solid #AEAEAE",
-                                    borderRadius: "8px",
-                                    padding: "4px",
-                                    margin: "5px",
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    color: "#000000",
-                                    backgroundColor: "#ffffff",
-                                    flexGrow: 1,
-                                  }}
-                                >
-                                  {projectName}
-                                </Typography>
-                              ))}
-                          </Grid>
-
+                          </Box>
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.noOfWorkingDays}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.totalWorkingDays}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.loggedInHours}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.ratings}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.variablePay}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "13px" }}>
+                          {row.lossOfPay}
+                        </TableCell>
+                        <TableCell style={{ fontSize: "16px" }}>
                           <Button
-                            variant="text"
-                            onClick={() => toggleProjectExpansion(index)}
-                            style={{ fontSize: "12px", color: "black" }}
+                            onClick={() => handleDownload(row.downloadLink)}
                           >
-                            <span style={{ marginRight: "5px" }}>
-                              View Less
-                            </span>
-                            <KeyboardArrowUpIcon
-                              style={{ color: "#008080", marginLeft: "5px" }}
-                            />
+                            <SimCardDownloadOutlinedIcon />
                           </Button>
-                        </Grid>
-                      </TableCell>
-                    </TableRow>
-                  ) : null}
-                </React.Fragment>
-              ))}
+                        </TableCell>
+                      </TableRow>
+                      {expandedProjects.includes(index) &&
+                      row.projectNames.length > 1 ? (
+                        <TableRow sx={{ border: "5px solid #EBEBEB" }}>
+                          <TableCell colSpan={12}>
+                            <Grid
+                              container
+                              alignItems="center"
+                              justifyContent="space-between"
+                              sx={{
+                                backgroundColor: "#fff",
+                                padding: "5px",
+                              }}
+                            >
+                              <Grid item display={"flex"} flexDirection={"row"}>
+                                {row.projectNames
+                                  .slice(1)
+                                  .map((projectName, index) => (
+                                    <Typography
+                                      key={index} // Don't forget to provide a unique key if you're rendering a list
+                                      variant="body1"
+                                      sx={{
+                                        border: "1px solid #AEAEAE",
+                                        borderRadius: "8px",
+                                        padding: "4px",
+                                        margin: "5px",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        color: "#000000",
+                                        backgroundColor: "#ffffff",
+                                        flexGrow: 1,
+                                      }}
+                                    >
+                                      {projectName}
+                                    </Typography>
+                                  ))}
+                              </Grid>
+
+                              <Button
+                                variant="text"
+                                onClick={() => toggleProjectExpansion(index)}
+                                style={{ fontSize: "12px", color: "black" }}
+                              >
+                                <span style={{ marginRight: "5px" }}>
+                                  View Less
+                                </span>
+                                <KeyboardArrowUpIcon
+                                  style={{
+                                    color: "#008080",
+                                    marginLeft: "5px",
+                                  }}
+                                />
+                              </Button>
+                            </Grid>
+                          </TableCell>
+                        </TableRow>
+                      ) : null}
+                    </React.Fragment>
+                  ))}
+                </>
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={12}>
+                    <Grid
+                      container
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center", 
+                        textAlign: "center",
+                        padding:"20px",
+                      }}
+                    >
+                      <Typography variant="h5" >No Data Available</Typography>
+                    </Grid>
+                  </TableCell>
+                </TableRow>
+              )}
             </TableBody>
           </Table>
           <Grid container justifyContent="flex-end" padding="10px">
