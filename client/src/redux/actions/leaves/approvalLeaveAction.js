f1a889e818213066f20d46a1d481d3e2a523083e
Yogita <yogita@prakat.in>
2024-07-16 20:41:11 +0530

    latest code

--- client/src/redux/actions/leaves/approvalLeaveAction.js
+++ client/src/redux/actions/leaves/approvalLeaveAction.js
@@ -197,19 +197,25 @@ export const downloadFileAction = (file, fileName) => {
   return async (dispatch) => {
     try {
       dispatch(downloadFileRequest());
-      const response = await downloadApi("GET", `${file}`);
-    
+      const response = await downloadApi("GET", `${file}`, { responseType: 'blob' });
 
-      const url = window.URL.createObjectURL(new Blob([response]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-      dispatch(downloadFileSuccess(response));
-      dispatch(downloadFileFail());
+      if (response.status === 200) {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        dispatch(downloadFileSuccess(response.data));
+      } else {
+        dispatch(downloadFileFail());
+      }
     } catch (err) {
-      // Handle errors
+      dispatch(downloadFileFail());
+      // Optionally handle errors here, e.g., dispatching a notification action
+      console.error("File download error:", err);
     }
   };
 };
+
